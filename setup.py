b41b41e1a4d779c6ab8826ed66675220850d9758
joaquin <joaquinmoine@gmail.com>
2018-02-16 08:13:02 -0300

    Add information in setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='smsc-python',
+    packages=['smsc-python'],
+    version='1.0',
+    description='Send SMS with https://www.smsc.com.ar/',
+    long_description=open('README.md').read(),
+    author='Joaquin Moine',
+    author_email='joaquinmoine@gmail.com',
+    url='https://github.com/joaquinmoine/smsc-python',
+    license=open('LICENSE').read(),
+    keywords=['sms', 'send'],
+    install_requires=requirements,
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License'
+    ],
+)
--- smsc/validations.py
+++ smsc/validations.py
@@ -4,11 +4,11 @@ from smsc.exceptions import AreaCodeSMSCError, LocalNumberSMSCError, PhoneNumber
 
 
 def validate_phone_number(area_code, local_number):
-    if re.match(r'^\d{2,4}$', area_code):
+    if not re.match(r'^\d{2,4}$', area_code):
         raise AreaCodeSMSCError(area_code, 'area_code: This param is invalid')
-    if re.match(r'^\d{6,8}$', local_number):
+    if not re.match(r'^\d{6,8}$', local_number):
         raise LocalNumberSMSCError(local_number, 'local_number: This param is invalid')
-    if re.match(r'^\d{10}$', area_code + local_number):
+    if not re.match(r'^\d{10}$', area_code + local_number):
         raise PhoneNumberLongSMSCError(area_code + local_number, 'The number should be 10 digits')
 
 
